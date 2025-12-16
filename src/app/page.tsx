16a1232b8e46fab9ecf82b5f6290fd3c1d790e2c
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2025-12-16 19:30:11 +0000

    feat: add initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Home() {
-	redirect("/items");
+	return (
+		<div className={styles.page}>
+			<div className={styles.welcomeContainer}>
+				<h1>🛍 Welcome to our store!</h1>
+				<p>
+					Remember, you can always ask our AI assistant if you need help finding
+					the perfect item. Just click the &quot;Ask me!&quot; button at the
+					bottom right corner.
+				</p>
+				<Link href="/items" className={styles.enterButton}>
+					Enter the store
+				</Link>
+			</div>
+		</div>
+	);
 }
